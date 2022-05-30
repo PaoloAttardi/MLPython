9e57497de3b9ac0934e46ad85d53066f46622f30
PaoloAttardi <81446798+PaoloAttardi@users.noreply.github.com>
2022-05-30 12:59:57 +0200

    Update data-exploration.py

--- Covid_project/data-exploration.py
+++ Covid_project/data-exploration.py
@@ -3,16 +3,24 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv('Covid_project/data_download_file_reference_2022.csv')
+df = pd.read_csv('MLPython/Covid_project/data_download_file_reference_2022.csv')
 # stat = df.describe()
 
 # il df permette tre diverse 'predizioni' una a bassa diffusione una a media e una ad alta diffusione del virus, si prende in esempio la media diffusione
 
 var = ['mean']
 
-colums_to_be_used = ['inf_' + var[0], 'seir_cumulative_' + var[0], 'inf_cuml_' + var[0],
-'seir_daily_' + var[0], 'seir_daily_unscaled_' + var[0], 'seir_cumulative_unscaled_' + var[0], 'Cases_' + var[0], ]
-
 filtered = df[df["location_name"] == 'Italy']
+df = filtered.get([
+    'date','inf_' + var[0], 'seir_cumulative_' + var[0], 'inf_cuml_' + var[0],
+    'seir_daily_' + var[0], 'seir_daily_unscaled_' + var[0], 'seir_cumulative_unscaled_' + var[0], 'cases_' + var[0], 'reff_' + var[0],
+    'cumulative_deaths', 'daily_deaths','cumulative_cases','cumulative_hospitalizations',
+    'daily_cases','population','mobility_' + var[0],'testing_'+ var[0],'pneumonia_'+ var[0],'mask_use_'+ var[0],'cumulative_all_vaccinated',
+    'cumulative_all_fully_vaccinated','hospital_beds_'+ var[0],'icu_beds_'+ var[0],'admis_'+ var[0],'all_bed_capacity','icu_bed_capacity',
+    'infection_fatality','infection_detection','infection_hospitalization'
+])
 
-print(filtered.head())
+sns.relplot(data=df, x='cumulative_all_fully_vaccinated', y='daily_cases', kind='line')
+sns.relplot(data=df, x='cumulative_all_fully_vaccinated', y='daily_deaths', kind='line')
+
+plt.show()
